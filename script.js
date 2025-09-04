bf26d57ed4faee237bba1ab48c6fd366a22291e7
Sim <sim.neodin@gmail.com>
2025-09-04 21:51:11 +0200

    Update script.js

--- script.js
+++ script.js
@@ -170,7 +170,7 @@
         i++;
         if(i<tokens.length){
           const delay=(/\s/.test(tokens[i-1]))?10:28;
-          setTimeout(step, delay * 20);
+          setTimeout(step, delay * 40);
         }else{
           // finished
           autoFit(box);
